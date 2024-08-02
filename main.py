4f8ebc26a9199209c6054b1aaac2623c240229d4
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-08-02 10:19:53 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+count_finished_homeworks = 12
+count_spent_hours = 1.5
+course_name = 'Python'
+time_for_one_task = count_spent_hours / count_finished_homeworks
+print('Курс: ' + course_name + ',', 'всего задач: ' + str(count_finished_homeworks) + ',', 'затрачено часов: '
+      + str(count_spent_hours) + ',', 'среднее время выполнения ' + str(time_for_one_task) + ' часа' + '.')
+
